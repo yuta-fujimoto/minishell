f2abe9ea5538230a6273ca7120254a844e93a82d
fyuta <fyuta@student.42tokyo.jp>
2021-09-25 19:15:48 +0900

    cd

--- srcs/builtin/cd_utils.c
+++ srcs/builtin/cd_utils.c
@@ -1,8 +1,8 @@
 #include "../../incs/minishell.h"
 
-char *update_environ_value(t_env *env, char *value)
+char	*update_environ_value(t_env *env, char *value)
 {
-	char *oldvalue;
+	char	*oldvalue;
 
 	if (!env || !value)
 		return (NULL);
@@ -11,27 +11,27 @@ char *update_environ_value(t_env *env, char *value)
 	return (oldvalue);
 }
 
-char *update_path(char **pathname, char *newcmp)
+char	*update_path(char **pathname, char *newcmp)
 {
-	char *tmp;
+	char	*tmp;
 
 	if (!newcmp)
 		return (*pathname);
-	if (*pathname)
-	{
-		tmp = *pathname;
-		*pathname = ft_strcjoin(*pathname, newcmp, '/');
-		free(tmp);
-	}
+	if (!(*pathname))
+		return (ft_strjoin("/", newcmp));
+	tmp = *pathname;
+	if (*pathname[ft_strlen(*pathname) - 1] == '/')
+		*pathname = ft_strjoin(*pathname, newcmp);
 	else
-		*pathname = ft_strjoin("/", newcmp);
+		*pathname = ft_strcjoin(*pathname, newcmp, '/');
+	free(tmp);
 	return (*pathname);
 }
 
-char *absolute_path(char *pathname)
+char	*absolute_path(char *pathname)
 {
-	char *current;
-	char *rlt;
+	char	*current;
+	char	*rlt;
 
 	current = get_current_directory();
 	if (!current)
@@ -44,85 +44,22 @@ char *absolute_path(char *pathname)
 	return (rlt);
 }
 
-bool is_valid_path(char **cmps, int end)
-{
-	char		*pathname;
-	struct stat	st;
-	int			i;
-
-	i = 0;
-	pathname = NULL;
-	while (i <= end)
-	{
-		if (!update_path(&pathname, cmps[i]))
-			return (false);
-		i++;
-	}
-	printf("%s\n", pathname);
-	if (stat(pathname, &st) == -1 || ((st.st_mode & S_IFMT) != S_IFLNK
-		&& (st.st_mode & S_IFMT) != S_IFDIR))
-	{
-		free(pathname);
-		return (false);
-	}
-	free(pathname);
-	return (true);
-}
-
-char	*canonical_path(char *pathname)
-{
-	char	**cmps;
-	char	*result;
-	int		i;
-
-	i = 0;
-	cmps = ft_split(pathname, '/');
-	if (!cmps)
-		return (NULL);
-	result = NULL;
-	while (cmps[i])
-	{
-		while (cmps[i] && cmps[i + 1] && ft_strncmp(cmps[i + 1], "..", 3) == 0 &&
-				ft_strncmp(cmps[i], "..", 3) != 0)
-		{
-			if (!is_valid_path(cmps, i))
-			{
-				errno = ENOENT;
-				free_str_arr(cmps);
-				return (NULL);
-			}
-			i += 2;
-		}
-		while (cmps[i] && ft_strncmp(cmps[i], ".", 2) == 0)
-			i++;
-		if (!cmps[i])
-			break ;
-		if (update_path(&result, cmps[i]) == NULL)
-		{
-			free_str_arr(cmps);
-			return (NULL);
-		}
-		i++;
-	}
-	if (!result)
-		return (ft_strdup("/"));
-	return (result);
-}
-
 int	set_working_directory(char *pathname)
 {
-	t_env *env;
-	char *oldpwd;
-	extern char **environ;
+	t_env		*env;
+	char		*oldpwd;
+	extern char	**environ;
+	char		*tmp;
 
 	env = environ_to_list();
 	oldpwd = update_environ_value(ft_find_env_var(env, "PWD"),
 			pathname);
 	if (!oldpwd)
 		oldpwd = ft_strdup("");
-	if (!update_environ_value(ft_find_env_var(env, "OLDPWD"), oldpwd))
-		if (!ft_envadd_back(&env, ft_envnew(ft_strdup("OLDPWD"), oldpwd)))
-			return (FAILURE);
+	tmp = update_environ_value(ft_find_env_var(env, "OLDPWD"), oldpwd);
+	if (!tmp && !ft_envadd_back(&env, ft_envnew(ft_strdup("OLDPWD"), oldpwd)))
+		return (FAILURE);
+	free(tmp);
 	free_environ();
 	environ = list_to_environ(env);
 	ft_envclear(&env, free);
--- srcs/builtin/ft_cd.c
+++ srcs/builtin/ft_cd.c
@@ -1,26 +1,30 @@
 #include "../../incs/minishell.h"
 
-char *get_available_path(char *pathname, bool *print_path)
+char	*get_available_path(char *pathname, bool *print_path)
 {
-	char *newpath;
-	char **cdpaths;
+	char	*newpath;
+	char	**cdpaths;
+	char	*rlt;
 
 	if (pathname[0] == '/')
-		return (canonical_path(pathname));
+		return (ft_strdup(pathname));
 	if (ft_strncmp(pathname, ".", 2) == 0 || ft_strncmp(pathname, "./", 2) == 0
-		|| ft_strncmp(pathname, "../", 3) == 0 || ft_strncmp(pathname, "..", 3) == 0)
-		return (canonical_path(absolute_path(pathname)));
+		|| ft_strncmp(pathname, "../", 3) == 0
+		|| ft_strncmp(pathname, "..", 3) == 0)
+		return (absolute_path(pathname));
 	cdpaths = ft_split(getenv("CDPATH"), ':');
 	if (!cdpaths)
-		return (canonical_path(absolute_path(pathname)));
+		return (absolute_path(pathname));
 	newpath = create_path(pathname, cdpaths);
 	if (!newpath)
-		return (canonical_path(absolute_path(pathname)));
+		return (absolute_path(pathname));
 	*print_path = true;
-	return (canonical_path(absolute_path(newpath)));
+	rlt = absolute_path(newpath);
+	free(newpath);
+	return (rlt);
 }
 
-int try_canonical_path(char *input)
+int	try_absolute_path(char *input)
 {
 	char	*pathname;
 	bool	print_path;
@@ -30,33 +34,46 @@ int try_canonical_path(char *input)
 	pathname = get_available_path(input, &print_path);
 	if (!pathname)
 		return (FAILURE);
-	if (!pathname)
-		return (FAILURE);
 	rlt = chdir(pathname);
 	if (rlt != 0)
+	{
+		free(pathname);
 		return (FAILURE);
+	}
 	if (print_path)
 		ft_putendl_fd(pathname, STDOUT_FILENO);
 	rlt = set_working_directory(pathname);
 	return (rlt);
 }
 
-int try_verbatim_path(char *input)
+int	try_verbatim_path(char *input)
 {
-	char *pathname;
+	char	*pathname;
 
-	pathname = absolute_path(input);
-	printf("%s\n", pathname);
-	if (pathname)
+	if (chdir(input) != 0)
 		return (FAILURE);
-	if (chdir(pathname) != 0)
-		return (FAILURE);
+	pathname = getcwd(NULL, 0);
+	if (!pathname)
+	{
+		ft_putendl_fd("cd: error retrieving current \
+directory: getcwd: cannot access parent \
+directories: No such file or directory", STDERR_FILENO);
+		pathname = getenv("PWD");
+		if (pathname[ft_strlen(pathname) - 1] == '/')
+			pathname = ft_strjoin(getenv("PWD"), input);
+		else
+			pathname = ft_strcjoin(getenv("PWD"), input, '/');
+		if (!pathname)
+			return (FAILURE);
+	}
 	return (set_working_directory(pathname));
 }
 
-int ft_cd_env(char *env)
+int	ft_cd_env(char *env)
 {
-	char *dirname;
+	char	*dirname;
+	int		rlt;
+	char	*err;
 
 	dirname = getenv(env);
 	if (!dirname)
@@ -66,15 +83,22 @@ int ft_cd_env(char *env)
 		ft_putendl_fd(" not set", STDERR_FILENO);
 		return (FAILURE);
 	}
-	chdir(dirname);
-	if (ft_strncmp(env, "OLDPWD", 7) == 0)
+	rlt = chdir(dirname);
+	if (rlt != 0)
+	{
+		err = ft_strjoin("minishell: cd ", dirname);
+		perror(err);
+		if (err)
+			free(err);
+	}
+	else if (ft_strncmp(env, "OLDPWD", 7) == 0)
 		ft_putendl_fd(dirname, STDOUT_FILENO);
-	return (SUCCESS);
+	return (set_working_directory(ft_strdup(dirname)));
 }
 
 int	ft_cd(char **av)
 {
-	char *err;
+	char	*err;
 
 	if (!av[1])
 		return (ft_cd_env("HOME"));
@@ -85,7 +109,7 @@ int	ft_cd(char **av)
 	}
 	if (ft_strncmp(av[1], "-", 2) == 0)
 		return (ft_cd_env("OLDPWD"));
-	if (try_canonical_path(av[1]) == SUCCESS)
+	if (try_absolute_path(av[1]) == SUCCESS)
 		return (SUCCESS);
 	if (av[1][0] != '/' && try_verbatim_path(av[1]) == SUCCESS)
 		return (SUCCESS);
