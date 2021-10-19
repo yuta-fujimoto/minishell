fdc197cdf15e709a65bcb103a0b6772bd92bfac3
fyuta <fyuta@student.42tokyo.jp>
2021-10-19 11:25:08 +0900

    add test.sh

--- test.sh
+++ test.sh
@@ -64,138 +64,141 @@ printf "| |  | |_| |_| |\  |_| |_ ____) | |  | | |____| |____| |____ \n"
 printf "|_|  |_|_____|_| \_|_____|_____/|_|  |_|______|______|______|\n$RESET"
 echo
 
-# ECHO TESTS
-exec_test 'echo test tout'
-exec_test 'echo test      tout'
-exec_test 'echo -n test tout'
-exec_test 'echo -n -n -n test tout'
-exec_test 'echo -nnnnnnnnnnnnnnnnnnnn test tout'
-exec_test 'echo -nnnnnnnnnnnnnfnnnnnnn test tout'
+# # ECHO TESTS
+# exec_test 'echo test tout'
+# exec_test 'echo test      tout'
+# exec_test 'echo -n test tout'
+# exec_test 'echo -n -n -n test tout'
+# exec_test 'echo -nnnnnnnnnnnnnnnnnnnn test tout'
+# exec_test 'echo -nnnnnnnnnnnnnfnnnnnnn test tout'
 
-# CD PWD TESTS
-exec_test 'cd .. ; pwd; env | grep PWD | sort'
-if [ $(uname) == "Linux" ]; then 
-	exec_test 'cd /home ; pwd; env | grep PWD | sort'
-else
-	exec_test 'cd /Users ; pwd; env | grep PWD | sort'
-fi
-exec_test 'cd ; pwd; env | grep PWD | sort'
-exec_test 'mkdir test_dir ; cd test_dir ; rm -rf ../test_dir ; cd . ; pwd ; cd . ; pwd ; cd .. ; pwd ; env | grep PWD | sort'
-exec_test 'cd fdfddffdfdfdff'
-# exec_test 'cd ./; unset OLDPWD ;cd ./; env | grep PWD' fix later,,,,,
-if [ $(uname) == "Linux" ]; then
-	exec_test 'export CDPATH=/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
-	exec_test 'export CDPATH=/home:/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
-fi
-#	exec_test 'export CDPATH=/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
-#	exec_test 'export CDPATH=/home:/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort'
-# replace with your minishell path!!
+# # CD PWD TESTS
+# exec_test 'cd .. ; pwd; env | grep PWD | sort'
+# if [ $(uname) == "Linux" ]; then 
+# 	exec_test 'cd /home ; pwd; env | grep PWD | sort'
+# else
+# 	exec_test 'cd /Users ; pwd; env | grep PWD | sort'
+# fi
+# exec_test 'cd ; pwd; env | grep PWD | sort'
+# exec_test 'mkdir test_dir ; cd test_dir ; rm -rf ../test_dir ; cd . ; pwd ; cd . ; pwd ; cd .. ; pwd ; env | grep PWD | sort'
+# exec_test 'cd fdfddffdfdfdff'
+# # exec_test 'cd ./; unset OLDPWD ;cd ./; env | grep PWD' fix later,,,,,
+# if [ $(uname) == "Linux" ]; then
+# 	exec_test 'export CDPATH=/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
+# 	exec_test 'export CDPATH=/home:/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
+# fi
+# #	exec_test 'export CDPATH=/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort' 
+# #	exec_test 'export CDPATH=/home:/home/fyuta/minishell/srcs; cd builtin; pwd; env | grep PWD | sort'
+# # replace with your minishell path!!
 
-exec_test 'cd'
+# exec_test 'cd'
 
-# export TESTS
-exec_test 'export AAA=BBB; env | grep AAA'
-exec_test 'export AAA=BBB; export AAA+=BBB; env | grep AAA'
-exec_test 'export AAA-=BBB; env | grep AAA'
-exec_test 'export AAA=; env | grep AAA'
-exec_test 'export 1AAA=; env | grep 1AAA'
-exec_test 'export A1AA=; env | grep A1AA'
-exec_test 'export A1AA; env | grep A1AA'
-exec_test 'export LLL'
-# echo "export > a | exit" | bash ; echo "export | grep -v "minishell >" > a | exit" | ./minishell > b | diff a b >> test_env.log | rm a b;
+# # export TESTS
+# exec_test 'export AAA=BBB; env | grep AAA'
+# exec_test 'export AAA=BBB; export AAA+=BBB; env | grep AAA'
+# exec_test 'export AAA-=BBB; env | grep AAA'
+# exec_test 'export AAA=; env | grep AAA'
+# exec_test 'export 1AAA=; env | grep 1AAA'
+# exec_test 'export A1AA=; env | grep A1AA'
+# exec_test 'export A1AA; env | grep A1AA'
+# exec_test 'export LLL'
+# # echo "export > a | exit" | bash ; echo "export | grep -v "minishell >" > a | exit" | ./minishell > b | diff a b >> test_env.log | rm a b;
 
-# unset TESTS
-exec_test 'export AAA=B; unset AAA ; env | grep AAA'
-exec_test 'export FFF=B; unset A B C D E FFF ; env | grep FFF'
-exec_test 'unset A B C D E F'
+# # unset TESTS
+# exec_test 'export AAA=B; unset AAA ; env | grep AAA'
+# exec_test 'export FFF=B; unset A B C D E FFF ; env | grep FFF'
+# exec_test 'unset A B C D E F'
 
-# env TESTS
-# echo "env > a | exit" | bash ; echo "env | grep -v "minishell > a" | exit" | ./minishell > b | diff a b >> test_env.log | rm a b;
+# # env TESTS
+# # echo "env > a | exit" | bash ; echo "env | grep -v "minishell > a" | exit" | ./minishell > b | diff a b >> test_env.log | rm a b;
 
-# PIPE TESTS
-exec_test 'echo test | cat -e | cat -e '
-# exec_test 'head -n 10 /dev/urandom  | wc ; wc -c'
-exec_test 'ls|ls|ls|ls|ls|ls|ls'
+# # PIPE TESTS
+# exec_test 'echo test | cat -e | cat -e '
+# # exec_test 'head -n 10 /dev/urandom  | wc ; wc -c'
+# exec_test 'ls|ls|ls|ls|ls|ls|ls'
 
-# ENV EXPANSIONS + ESCAPE
-exec_test 'echo test     \    test'
-exec_test 'echo \"test'
-exec_test 'echo $TEST'
-exec_test 'echo "$TEST"'
-exec_test "echo '$TEST'"
-exec_test 'echo "$TEST$TEST$TEST"'
-exec_test 'echo "$TEST$TEST=lol$TEST"'
-exec_test 'echo "   $TEST lol $TEST"'
-exec_test 'echo $TEST$TEST$TEST'
-exec_test 'echo $TEST$TEST=lol$TEST""lol'
-exec_test 'echo    $TEST lol $TEST'
-exec_test 'echo test "" test "" test'
-exec_test 'echo "\$TEST"'
-exec_test 'echo "$=TEST"'
-exec_test 'echo "$"'
-exec_test 'echo "$?TEST"'
-exec_test 'echo $TEST $TEST'
-exec_test 'echo "$1TEST"'
-exec_test 'echo "$T1TEST"'
-exec_test 'echo "''''"'
+# # ENV EXPANSIONS + ESCAPE
+# exec_test 'echo test     \    test'
+# exec_test 'echo \"test'
+# exec_test 'echo $TEST'
+# exec_test 'echo "$TEST"'
+# exec_test "echo '$TEST'"
+# exec_test 'echo "$TEST$TEST$TEST"'
+# exec_test 'echo "$TEST$TEST=lol$TEST"'
+# exec_test 'echo "   $TEST lol $TEST"'
+# exec_test 'echo $TEST$TEST$TEST'
+# exec_test 'echo $TEST$TEST=lol$TEST""lol'
+# exec_test 'echo    $TEST lol $TEST'
+# exec_test 'echo test "" test "" test'
+# exec_test 'echo "\$TEST"'
+# exec_test 'echo "$=TEST"'
+# exec_test 'echo "$"'
+# exec_test 'echo "$?TEST"'
+# exec_test 'echo $TEST $TEST'
+# exec_test 'echo "$1TEST"'
+# exec_test 'echo "$T1TEST"'
+# exec_test 'echo "''''"'
 
-# # ENV EXPANSIONS
-# ENV_SHOW="env | sort | grep -v SHLVL | grep -v _="
-# EXPORT_SHOW="export | sort | grep -v SHLVL | grep -v _= | grep -v OLDPWD"
-# exec_test 'export ='
-# exec_test 'export 1TEST= ;' $ENV_SHOW
-# exec_test 'export TEST ;' $EXPORT_SHOW
-# exec_test 'export ""="" ; ' $ENV_SHOW
-# exec_test 'export TES=T="" ;' $ENV_SHOW
-# exec_test 'export TE+S=T="" ;' $ENV_SHOW
-# exec_test 'export TEST=LOL ; echo $TEST ;' $ENV_SHOW
-exec_test '$NO'
-exec_test 'export TEST=LOL ; echo $TEST$TEST$TEST=lol$TEST'
-# exec_test 'export TEST=LOL; export TEST+=LOL ; echo $TEST ;' $ENV_SHOW
-# exec_test $ENV_SHOW
-# exec_test $EXPORT_SHOW
-exec_test 'export TEST="ls       -l     - a" ; echo $TEST '
+# # # ENV EXPANSIONS
+# # ENV_SHOW="env | sort | grep -v SHLVL | grep -v _="
+# # EXPORT_SHOW="export | sort | grep -v SHLVL | grep -v _= | grep -v OLDPWD"
+# # exec_test 'export ='
+# # exec_test 'export 1TEST= ;' $ENV_SHOW
+# # exec_test 'export TEST ;' $EXPORT_SHOW
+# # exec_test 'export ""="" ; ' $ENV_SHOW
+# # exec_test 'export TES=T="" ;' $ENV_SHOW
+# # exec_test 'export TE+S=T="" ;' $ENV_SHOW
+# # exec_test 'export TEST=LOL ; echo $TEST ;' $ENV_SHOW
+# exec_test '$NO'
+# exec_test 'export TEST=LOL ; echo $TEST$TEST$TEST=lol$TEST'
+# # exec_test 'export TEST=LOL; export TEST+=LOL ; echo $TEST ;' $ENV_SHOW
+# # exec_test $ENV_SHOW
+# # exec_test $EXPORT_SHOW
+# exec_test 'export TEST="ls       -l     - a" ; echo $TEST '
 
-# # REDIRECTIONS
-exec_test 'echo test > ls ; cat ls'
-exec_test 'echo test > ls >> ls >> ls ; echo test >> ls; cat ls'
-exec_test '> lol echo test lol; cat lol'
-exec_test '>lol echo > test>lol>test>>lol>test mdr >lol test >test; cat test'
-exec_test 'cat < ls'
-exec_test 'cat < ls > ls'
+# # # REDIRECTIONS
+# exec_test 'echo test > ls ; cat ls'
+# exec_test 'echo test > ls >> ls >> ls ; echo test >> ls; cat ls'
+# exec_test '> lol echo test lol; cat lol'
+# exec_test '>lol echo > test>lol>test>>lol>test mdr >lol test >test; cat test'
+# exec_test 'cat < ls'
+# exec_test 'cat < ls > ls'
 
-# REDIRECTIONS + EXPANSIONS
-exec_test 'echo $HOME > test; cat test'
-exec_test 'echo a b c d e f g > test; < test cat'
-exec_test 'echo test > $NO'
-exec_test 'cat < $NO'
-exec_test '> test echo $HOME'
-exec_test '> $NO echo test'
+# # REDIRECTIONS + EXPANSIONS
+# exec_test 'echo $HOME > test; cat test'
+# exec_test 'echo a b c d e f g > test; < test cat'
+# exec_test 'echo test > $NO'
+# exec_test 'cat < $NO'
+# exec_test '> test echo $HOME'
+# exec_test '> $NO echo test'
 
-# # MULTI TESTS
-# exec_test 'echo testing multi ; echo "test 1 ; | and 2" ; cat tests/lorem.txt | grep Lorem'
+# # # MULTI TESTS
+# # exec_test 'echo testing multi ; echo "test 1 ; | and 2" ; cat tests/lorem.txt | grep Lorem'
 
-# # SYNTAX ERROR
-# exec_test ';; test'
-# exec_test '| test'
-# exec_test 'echo > <'
-# exec_test 'echo | |'
-# exec_test '<'
+# # # SYNTAX ERROR
+# # exec_test ';; test'
+# # exec_test '| test'
+# # exec_test 'echo > <'
+# # exec_test 'echo | |'
+# # exec_test '<'
 
-# # EXIT
-exec_test "exit 42"
-# exec_test "exit 42 53 68"
-exec_test "exit 259"
-exec_test "exit 9223372036854775807"
-exec_test "exit -9223372036854775808"
-exec_test "exit 9223372036854775808"
-exec_test "exit -9223372036854775810"
-exec_test "exit -4"
-exec_test "exit wrong"
-exec_test "exit wrong_command"
-exec_test "gdagadgag"
-exec_test "ls -Z"
-exec_test "cd gdhahahad"
-exec_test "ls -la | wtf"
+# # # EXIT
+# exec_test "exit 42"
+# # exec_test "exit 42 53 68"
+# exec_test "exit 259"
+# exec_test "exit 9223372036854775807"
+# exec_test "exit -9223372036854775808"
+# exec_test "exit 9223372036854775808"
+# exec_test "exit -9223372036854775810"
+# exec_test "exit -4"
+# exec_test "exit wrong"
+# exec_test "exit wrong_command"
+# exec_test "gdagadgag"
+# exec_test "ls -Z"
+# exec_test "cd gdhahahad"
+# exec_test "ls -la | wtf"
+exec_test "touch a; chmod 355 a; cat a"
+exec_test "touch b; chmod 355 b; < b cat"
+exec_test "touch c; chmod 355 c; c"
 
-rm lol ls test test_stdout
+rm lol ls test test_stdout a b c
