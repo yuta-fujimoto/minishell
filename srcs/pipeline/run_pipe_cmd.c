dccdaf7a2826080831a4f23481901bcadba4053a
AuroraSloan <lanajadethompson@gmail.com>
2021-10-23 11:52:31 +0900

    fix integration

--- srcs/pipeline/run_pipe_cmd.c
+++ srcs/pipeline/run_pipe_cmd.c
@@ -2,17 +2,12 @@
 
 extern t_sig_info	g_sig_info;
 
-static bool	end_pipe(t_node *n, t_pipe_info *p_info, bool aftr_chld, int rlt)
+static bool	end_pipe(t_node *n, t_pipe_info *p_info, int rlt)
 {
 	if (p_info->cmd_path)
 		free(p_info->cmd_path);
 	if (p_info->cmd && has_redirection(n))
 		free(p_info->cmd);
-	if (aftr_chld)
-	{
-		if (has_redirection(n))
-			rlt = end_redirection(NULL, p_info->rdr, rlt);
-	}
 	return (expansion_node_conclude(n, rlt));
 }
 
@@ -23,8 +18,8 @@ static int	pipe_command_not_found(t_node *n, t_pipe_info *p_info, t_pipes *pipes
 	ft_putendl_fd(": command not found" , STDERR_FILENO);
 	g_sig_info.exit_status = 127;
 	if (!ft_pidlstadd_back(&pipes->pidlst, ft_pidlstnew(0)))
-		return (end_pipe(n, p_info, false, FAILURE));
-	return (end_pipe(n, p_info, false, SUCCESS));
+		return (end_pipe(n, p_info, FAILURE));
+	return (end_pipe(n, p_info, SUCCESS));
 }
 
 static int	get_cmd_path(char **new_cmd, char **cmd_path)
@@ -45,7 +40,10 @@ static bool	init_pipe_cmd(t_node *exp_node, t_pipe_info *p_info, t_redir *redir)
 	p_info->touch = false;
 	p_info->cmd = NULL;
 	p_info->cmd_path = NULL;
-	p_info->cmd = get_cmd(exp_node, redir, &p_info->touch);
+	if (has_redirection(exp_node))
+		p_info->cmd = create_new_cmd(exp_node, &p_info->touch);
+	else
+		p_info->cmd = exp_node->av;
 	if (!p_info->cmd && !p_info->touch && has_redirection(exp_node))
 		return (FAILURE);
 	if (get_cmd_path(p_info->cmd, &p_info->cmd_path) == FAILURE)
@@ -65,18 +63,18 @@ bool	run_pipe_cmd(t_node node, t_pipes *pipes, t_set *set, t_redir *redir)
 	if (!exp_node->av)
 		return (expansion_node_conclude(exp_node, SUCCESS));
 	if (init_pipe_cmd(exp_node, &p_info, redir) == FAILURE)
-		return (end_pipe(exp_node, &p_info, false, FAILURE));
+		return (end_pipe(exp_node, &p_info, FAILURE));
 	if (!p_info.cmd_path && !is_buildin(node.av[0]))
 		return (pipe_command_not_found(exp_node, &p_info, pipes));
 	c_pid = fork();
 	if (c_pid < 0)
-		return (end_pipe(exp_node, &p_info, false, FAILURE));
+		return (end_pipe(exp_node, &p_info, FAILURE));
 	else if (c_pid == 0)
 		run_child(exp_node, pipes, set, &p_info);
 	else
 	{
 		if (!ft_pidlstadd_back(&pipes->pidlst, ft_pidlstnew(c_pid)))
-			return (end_pipe(exp_node, &p_info, true, FAILURE));
+			return (end_pipe(exp_node, &p_info, FAILURE));
 	}
-	return (end_pipe(exp_node, &p_info, true, SUCCESS));
+	return (end_pipe(exp_node, &p_info, SUCCESS));
 }
